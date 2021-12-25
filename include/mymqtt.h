834c1472193db2cbded5aa0d7f19d2642d6d6729
Pierre Fromager <info@pier-infor.fr>
2021-12-25 09:05:28 +0100

    feat : myMqtt

---
+++ include/mymqtt.h
@@ -0,0 +1,53 @@
+
+#ifndef APP_MYMQTT_H
+#define APP_MYMQTT_H
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <mosquittopp.h>
+
+using namespace std;
+
+class myMqtt : public mosqpp::mosquittopp
+{
+
+public:
+    myMqtt(
+        string id,
+        string publish_topic,
+        vector<string> subscribed_topics,
+        string host,
+        int port);
+    myMqtt(
+        string id,
+        string publish_topic,
+        vector<string> subscribed_topics,
+        string host,
+        int port,
+        string username,
+        string password);
+    ~myMqtt();
+    bool publish(string message);
+    bool subscribe();
+
+private:
+    string m_id;
+    string m_publish_topic;
+    vector<string> m_subscribed_topics;
+    string m_host;
+    int m_port;
+    string m_username;
+    string m_password;    
+    const int m_keepalive = 60;
+    const int m_qos = 0;
+    const bool m_retain = false;
+    void on_connect(int rc);
+    void on_disconnect(int rc);
+    void on_publish(int mid);
+    void on_subscribe(int mid, int qos_count, const int *granted_qos);
+    void on_message(const struct mosquitto_message *message);
+};
+
+#endif
---
+++ src/mymqtt.cpp
@@ -0,0 +1,106 @@
+
+#include "mymqtt.h"
+
+myMqtt::myMqtt(
+    string id,
+    string publish_topic,
+    vector<string> subscribed_topics,
+    string host,
+    int port) : mosquittopp(id.c_str()),
+                m_id(id),
+                m_publish_topic(publish_topic),
+                m_subscribed_topics(subscribed_topics),
+                m_host(host),
+                m_port(port)
+{
+    mosqpp::lib_init();
+    connect_async(m_host.c_str(), m_port, m_keepalive);
+    loop_start();
+}
+
+myMqtt::myMqtt(
+    string id,
+    string publish_topic,
+    vector<string> subscribed_topics,
+    string host,
+    int port,
+    string username,
+    string password) : mosquittopp(id.c_str()),
+                       m_id(id),
+                       m_publish_topic(publish_topic),
+                       m_subscribed_topics(subscribed_topics),
+                       m_host(host),
+                       m_port(port),
+                       m_username(username),
+                       m_password(password)
+
+{
+    mosqpp::lib_init();
+    mosquittopp::username_pw_set(m_username.c_str(), m_password.c_str());
+    connect_async(m_host.c_str(), m_port, m_keepalive);
+    loop_start();
+}
+
+myMqtt::~myMqtt()
+{
+    disconnect();
+    loop_stop();
+    mosqpp::lib_cleanup();
+}
+
+bool myMqtt::publish(string message)
+{
+    int answer = mosqpp::mosquittopp::publish(
+        nullptr,
+        m_publish_topic.c_str(),
+        message.length(),
+        message.c_str(),
+        m_qos,
+        m_retain);
+    return (answer == MOSQ_ERR_SUCCESS);
+}
+
+bool myMqtt::subscribe()
+{
+    bool success = true;
+    const unsigned int &numberOfTopics = m_subscribed_topics.size();
+    unsigned int i;
+    for (i = 0; i < numberOfTopics; i++)
+    {
+        int answer = mosquittopp::subscribe(nullptr, m_subscribed_topics[i].c_str());
+        if (answer != MOSQ_ERR_SUCCESS)
+        {
+            success = false;
+        }
+    }
+    return success;
+}
+
+void myMqtt::on_subscribe(int mid, int qos_count, const int *granted_qos)
+{
+    cout << "Subscription succeeded." << endl;
+}
+
+void myMqtt::on_message(const struct mosquitto_message *message)
+{
+    string payload = string(static_cast<char *>(message->payload));
+    string topic = string(message->topic);
+    cout << "payload: " << payload << endl;
+    cout << "topic: " << topic << endl;
+}
+
+void myMqtt::on_disconnect(int rc)
+{
+    cout << "disconnection(" << rc << ")" << endl;
+}
+
+void myMqtt::on_connect(int rc)
+{
+    const string &status = (rc == 0) ? "success" : "fail";
+    cout << "connect " << status << " (" << rc << ")" << endl;
+}
+
+void myMqtt::on_publish(int mid)
+{
+    cout << "Message (" << mid << ") succeed to be published " << endl;
+}
