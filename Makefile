518aed8e3aff92d51765ecac8bb773c60c4d5bb6
Pierre Fromager <info@pier-infor.fr>
2022-01-01 16:41:34 +0100

    sec : increase security compilers directives

--- Makefile
+++ Makefile
@@ -1,6 +1,27 @@
 CXX			= g++
-CXXFLAGS	= -g -Werror -Wall -Wextra -Wpedantic -std=c++17 -I./include \
-	-Wno-unused-function -Wno-unused-parameter -Wno-unused-variable
+CXXFLAGS	= -O2 -Werror -Wall -Wextra -Wpedantic -std=c++17 \
+	-I./include \
+	-Wno-unused-function -Wno-unused-parameter -Wno-unused-variable \
+	-Wno-format-nonliteral \
+	-Wformat=2 -Wformat-security \
+	-Wnull-dereference -Wstack-protector -Wtrampolines -Wvla \
+	-Warray-bounds=2 \
+	-Wshift-overflow=2 \
+	-Wlogical-op -Wduplicated-cond \
+	-Wformat-signedness -Wshadow -Wstrict-overflow=4 \
+	-Wundef -Wswitch-enum \
+	-Wstack-usage=10000 \
+	-D_FORTIFY_SOURCE=2 
+
+# Could be added for wxwidget > 3.0	
+#-Wcast-qual  -Wconversion \
+
+# Could be added for recent compilers
+#-fstack-protector-strong -fPIE -fsanitize=address \
+#-fsanitize=leak -fno-omit-frame-pointer -fsanitize=undefined \
+#-fsanitize=bounds-strict -fsanitize=float-divide-by-zero \
+#-fsanitize=float-cast-overflow
+
 LDFLAGS		= -g -s
 LDLIBS		= -lm -lmosquittopp
 
@@ -28,22 +49,24 @@ $(TARGET): $(OBJECTS)
 
 .PHONY: doc
 doc:
-	doxygen $(DOC)/wxtest.conf
+	@doxygen $(DOC)/wxtest.conf
 
 .PHONY: cleandoc
 cleandoc:
-	rm -rf $(DOC)/html	
+	@rm -rf $(DOC)/html
+	@echo "clean doc done"
 
 .PHONY: clean
 clean:
-	rm -rf $(TARGET) $(OBJECTS)  
+	@rm -rf $(TARGET) $(OBJECTS)
+	@echo "clean done"
 
 .PHONY: check
 check:
-	cppcheck -I./include \
+	@cppcheck -I./include \
 		--check-config \
 		--enable=all \
 		--std=c++11 \
 		--suppress=missingIncludeSystem \
-		$(SRC) -I $(SRC)	
+		$(SRC)	
 
--- buildw32.cmd
+++ buildw32.cmd
@@ -1,20 +1,47 @@
+@CLS
+@SET CPPFLAGS=-O2 -Werror -Wall -Wextra -Wpedantic -std=c++17 ^
+	-mwindows -g -s ^
+	-Wno-unused-function -Wno-unused-parameter -Wno-unused-variable ^
+	-lm ^
+	-Wno-format-nonliteral ^
+	-Wformat=2 -Wformat-security ^
+	-Wnull-dereference -Wstack-protector -Wtrampolines -Wvla ^
+	-Warray-bounds=2 ^
+	-Wshift-overflow=2 -Wcast-qual ^
+	-Wconversion -Wlogical-op -Wduplicated-cond ^
+	-Wformat-signedness -Wshadow -Wstrict-overflow=4 ^
+	-Wundef -Wswitch-enum ^
+	-Wstack-usage=10000 ^
+	-D_FORTIFY_SOURCE=2 ^
+    -fstack-protector-strong -fPIE -fsanitize=address ^
+	-fsanitize=leak -fno-omit-frame-pointer -fsanitize=undefined ^
+	-fsanitize=bounds-strict -fsanitize=float-divide-by-zero ^
+	-fsanitize=float-cast-overflow
 
-SET CPP_FILES=".\src\*.cpp"
-SET BUILD_OUPUT=".\build\wxtest"
-SET LOCAL_INC=".\include"
-SET WX_BASE_PATH=C:\Users\pierre\Downloads\wxWidget\
-SET MQTT_BASE_PATH=C:\Users\pierre\Downloads\mosquitto-2.0.14
-SET MOSQ_LIB="%MQTT_BASE_PATH%\include"
-SET MQTT_LIB="%MQTT_BASE_PATH%\lib"
-SET MQTT_LIB_CPP="%MQTT_BASE_PATH%\lib\cpp"
-SET MOSQ_BUILD_LIB_CPP="%MQTT_BASE_PATH%\build\lib\cpp"
-SET WX_HEADERS_INC="%WX_BASE_PATH%wxWidgets-3.1.5-headers\include"
-SET WX_DLL_DEV=wxMSW-3.1.5_gcc1020_x64_Dev\lib\gcc1020_x64_dll
-SET WX_MSWUD="%WX_BASE_PATH%%WX_DLL_DEV%\mswud"
-SET WX_DLL_PATH="%WX_BASE_PATH%%WX_DLL_DEV%"
+@SET CPP_FILES=".\src\*.cpp"
+@SET BUILD_OUPUT=".\build\wxtest"
+@SET LOCAL_INC=".\include"
+@SET WX_BASE_PATH=C:\Users\pierre\Downloads\wxWidget\
+@SET MQTT_BASE_PATH=C:\Users\pierre\Downloads\mosquitto-2.0.14
+@SET MOSQ_LIB="%MQTT_BASE_PATH%\include"
+@SET MQTT_LIB="%MQTT_BASE_PATH%\lib"
+@SET MQTT_LIB_CPP="%MQTT_BASE_PATH%\lib\cpp"
+@SET MOSQ_BUILD_LIB_CPP="%MQTT_BASE_PATH%\build\lib\cpp"
+@SET WX_HEADERS_INC="%WX_BASE_PATH%wxWidgets-3.1.5-headers\include"
+@SET WX_DLL_DEV=wxMSW-3.1.5_gcc1020_x64_Dev\lib\gcc1020_x64_dll
+@SET WX_MSWUD="%WX_BASE_PATH%%WX_DLL_DEV%\mswud"
+@SET WX_DLL_PATH="%WX_BASE_PATH%%WX_DLL_DEV%"
 
-g++ %CPP_FILES% ^
-    -mwindows ^
+@ECHO -- Build start --
+@ECHO - Headers pathes :
+@ECHO  * wxwidget : %WX_HEADERS_INC%
+@ECHO  * mosquitto : %MOSQ_LIB%
+@ECHO - Lib pathes :
+@ECHO  * wxwidget : %WX_DLL_PATH%
+@ECHO  * mosquitto : %MOSQ_BUILD_LIB_CPP%
+
+@g++ %CPP_FILES% ^
+    %CPPFLAGS% ^
     -o %BUILD_OUPUT% ^
     -I %LOCAL_INC% ^
     -I %MOSQ_LIB% ^
@@ -27,3 +54,5 @@ g++ %CPP_FILES% ^
     -l "wxmsw31u_core" ^
     -L %MOSQ_BUILD_LIB_CPP% ^
     -l "mosquittopp"
+
+@ECHO -- Build end --
